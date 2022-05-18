e2c01d11463fda5920c8f5448cf73c9665a95308
nk400 <navkr007@gmail.com>
2022-05-18 19:47:05 +0530

    ADDED: Fundamemtals

---
+++ src/com/bl/basics/Main.java
@@ -0,0 +1,93 @@
+package com.bl.basics;
+
+
+import java.lang.reflect.Array;
+import java.util.Date;
+
+public class Main {
+    byte a = 12;
+    short b = 123;
+    int c = 1123;
+    long d = 34335454545454l;
+    float e = 2.4f; // automatic double but specify f to get float
+    double f = 2.4; // defalut double
+    char g = 'u'; // char litral
+    boolean h = true;
+    String i = "hi"; //string litral
+
+
+    //non premitive data type
+    // class, interface
+    int[] intArr = {1, 2, 3};
+    //wrapper data types
+
+    Byte j = 12;
+    Short k = 2123;
+    Integer l = 1234;
+    Long m = 35454544l;
+    Float n = 4.7f;
+    Double o = 2.5454545;
+    Character p = '&';
+    Boolean bz = false;
+
+    Array arrObj;
+    Date date;
+
+    static long add(int a, int b) {
+        return a + b;
+    }
+
+    static boolean isEvenNumber(long l) {
+        boolean ansz = l % 2 == 0;
+        return ansz;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to java basics");
+        //Bird parrot = new Bird();
+       /* parrot.sound = "squake";
+        parrot.species = "anything";
+        parrot.type = "non-mamal";*/
+
+        int sum = (int) add(2344, 454);
+        System.out.println(sum);
+        boolean b = isEvenNumber(45);
+        System.out.println(b);
+
+        Bird parrot1 = new Parrot();
+
+    }
+}
+
+abstract class Bird {
+    String sound;
+    String color;
+    String name;
+    String species;
+    String type;
+
+    abstract void eat();
+
+    void fly() {
+    }
+
+    void swim() {
+    }
+}
+
+class Parrot extends Bird {
+
+    String extra;
+
+    void extraFun() {
+
+    }
+
+    @Override
+    void eat() {
+        System.out.println();
+    }
+}
+
+
